1bd6bb29e14367abc32ad16faa7095c6eabfce2a
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-06-26 11:57:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Function to return a promise after 3 seconds
 
 
-function getData() {
+/* function getData() {
     return new Promise(resolve => {
         setTimeout(() => {
             resolve([1, 2, 3, 4]);
